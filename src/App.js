38bb1b175448678ff66e794ae997de1cfac087f8
govindam-lpu <govindam.12003032@lpu.in>
2023-02-12 01:12:29 +0530

    minor chages

--- src/App.js
+++ src/App.js
@@ -1,98 +1,19 @@
 import { useEffect, useState } from 'react';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Link
-// } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import AddMember from './AddMember';
 import './App.css';
+import MemberList from './MemberList';
 
 function App() {
 
   const [value, setValue] = useState([]);
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState();
-  const [email, setEmail] = useState('');
-  const [address, setAddress] = useState('');
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const details = {
-      name,
-      phone,
-      email,
-      address
-    };
-
-    setValue((prev) => {
-      return [
-        ...prev,
-        details
-      ]
-    });
-    setName('');
-    setEmail('');
-    setPhone(null);
-    setAddress('');
-  };
-
-  useEffect(() => console.log(value), [value]);
 
   return (
-    <div className="">
-      {/* <Routes>
+    <Routes>
+      <Route path="/" element={<AddMember setValue={setValue}  />} />
+      <Route path="/members" element={<MemberList value={value}  />} />
+    </Routes>
 
-        <Route exact path='/members' element={< MemberList />}></Route>
-      </Routes> */}
-      <form onSubmit={onSubmit}>
-        <div class="form-group">
-          <label>Name</label>
-          <input 
-            type="text" 
-            className="form-control"  
-            placeholder="Enter name"
-            value={name}
-            onChange={e => setName(e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Email address</label>
-          <input 
-            type="email" 
-            className="form-control"  
-            placeholder="Enter email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Phone Number</label>
-          <input 
-            type="phone" 
-            className="form-control" 
-            placeholder="Phone"
-            value={phone}
-            onChange={e => setPhone(e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Address</label>
-          <textarea 
-            className="form-control" 
-            placeholder="Address"
-            value={address}
-            onChange={e => setAddress(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Add Member</button>
-      </form>
-
-      {/* <Link to='/members'>
-        <button 
-          className="btn btn-primary">View Member List
-        </button>
-      </Link> */}
-    </div>
   );
 }
 
--- src/MemberList.js
+++ src/MemberList.js
@@ -1,8 +1,36 @@
 import React from 'react'
 
-const MemberList = () => {
+const MemberList = ({ value }) => {
+
+  console.log(value)
+
+  const data = value?.map((el, i) => {
+    return (
+      <tr key={i}>
+        <th scope="row">{i}</th>
+        <td>{el.name}</td>
+        <td>{el.email}</td>
+        <td>{el.phone}</td>
+        <td>{el.address}</td>
+      </tr>
+    )
+  })
+
   return (
-    <div>MemberList</div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Phone Number</th>
+          <th scope="col">Address</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data}
+      </tbody>
+    </table>
   )
 }
 
